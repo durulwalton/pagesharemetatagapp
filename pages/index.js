696a20e47e03b9dabd1e01c693e99937b50e90ae
durulwalton <durulwalton@gmail.com>
2022-12-26 14:27:36 +0600

    forth commit

--- pages/index.js
+++ pages/index.js
@@ -6,45 +6,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>বিশ্বকাপ ফুটবল থেকে বাংলাদেশ কী পেল | প্রথম আলো</title>
+        <title>Talha</title>
         <meta
           name="title"
-          content="বিশ্বকাপ ফুটবল থেকে বাংলাদেশ কী পেল | প্রথম আলো"
+          content="new বিশ্বকাপ ফুটবল থেকে বাংলাদেশ কী পেল  | প্রথম আলো"
         />
         <meta
           name="description"
-          content="ভৌগোলিকভাবে ২০২২ সালেই বাংলাদেশের সবচেয়ে কাছে অনুষ্ঠিত হয়েছে বিশ্বকাপ ফুটবলের আসর। কোভিড–পরবর্তী দুনিয়া, ইউক্রেনের যুদ্ধ, বিশ্বরাজনীতিতে শক্তির ভারসাম্য পরিবর্তনের পটভূমিতে প্রথমবারের মতো মুসলিম কোনো দেশে আয়োজিত এই বিশ্বকাপের দিকে খেলার বাইরেও অন্যান্য বিষয়ে সবার নজর ছিল বেশি।"
+          content="New ভৌগোলিকভাবে ২০২২ সালেই বাংলাদেশের সবচেয়ে কাছে অনুষ্ঠিত হয়েছে বিশ্বকাপ ফুটবলের আসর। কোভিড–পরবর্তী দুনিয়া, ইউক্রেনের যুদ্ধ, বিশ্বরাজনীতিতে শক্তির ভারসাম্য পরিবর্তনের পটভূমিতে প্রথমবারের মতো মুসলিম কোনো দেশে আয়োজিত এই বিশ্বকাপের দিকে খেলার বাইরেও অন্যান্য বিষয়ে সবার নজর ছিল বেশি।"
         />
-        <meta
-          name="keywords"
-          content="কাতার বিশ্বকাপ ২০২২,বিশ্বকাপ ফুটবল ২০২২,বিশেষ সংবাদ,আর্জেন্টিনা ফুটবল দল,মেসি,ব্রাজিল"
-        />
-        <meta name="author" content="লেখা: " />
-        <meta
-          name="news_keywords"
-          content="কাতার বিশ্বকাপ ২০২২,বিশ্বকাপ ফুটবল ২০২২,বিশেষ সংবাদ,আর্জেন্টিনা ফুটবল দল,মেসি,ব্রাজিল"
-        />
-        <meta property="og:type" content="article" />
         <meta
           property="og:title"
           content="বিশ্বকাপ ফুটবল থেকে বাংলাদেশ কী পেল"
         />
+        <meta property="og:type" content="article" />
         <meta
           property="og:description"
           content="ভৌগোলিকভাবে ২০২২ সালেই বাংলাদেশের সবচেয়ে কাছে অনুষ্ঠিত হয়েছে বিশ্বকাপ ফুটবলের আসর। কোভিড–পরবর্তী দুনিয়া, ইউক্রেনের যুদ্ধ, বিশ্বরাজনীতিতে শক্তির ভারসাম্য পরিবর্তনের পটভূমিতে প্রথমবারের মতো মুসলিম কোনো দেশে আয়োজিত এই বিশ্বকাপের দিকে খেলার বাইরেও অন্যান্য বিষয়ে সবার নজর ছিল বেশি।"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="বিশ্বকাপ ফুটবল থেকে বাংলাদেশ কী পেল"
-        />
-        <meta
-          name="twitter:description"
-          content="ভৌগোলিকভাবে ২০২২ সালেই বাংলাদেশের সবচেয়ে কাছে অনুষ্ঠিত হয়েছে বিশ্বকাপ ফুটবলের আসর। কোভিড–পরবর্তী দুনিয়া, ইউক্রেনের যুদ্ধ, বিশ্বরাজনীতিতে শক্তির ভারসাম্য পরিবর্তনের পটভূমিতে প্রথমবারের মতো মুসলিম কোনো দেশে আয়োজিত এই বিশ্বকাপের দিকে খেলার বাইরেও অন্যান্য বিষয়ে সবার নজর ছিল বেশি।"
-        />
-        <meta
-          name="twitter:image"
-          content="https://images.prothomalo.com/prothomalo-bangla%2F2022-12%2Ff15d1dae-0e2d-4d10-96f4-b70fc636a557%2FWHATSAPP_IMAGE_2022_12_25_AT_113130_PM_1113306.gif?rect=0%2C170%2C2400%2C1260&amp;w=1200&amp;ar=40%3A21&amp;auto=format%2Ccompress&amp;ogImage=true&amp;mode=crop&amp;overlay=https%3A%2F%2Fimages.prothomalo.com%2Fprothomalo-bangla%2F2022-01%2F18c58c05-9e66-46f3-8946-4437460f8f90%2FBanner_7814X143.jpg&amp;overlay_position=bottom&amp;overlay_width_pct=1"
         />
         <meta
           property="og:image"
@@ -52,12 +30,10 @@ export default function Home() {
         />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="fb:app_id" content="1499138263726489" />
-        <meta name="brand_name" content="প্রথম আলো" />
       </Head>
 
       <main className={styles.main}>
-        <h1>বাংলাদেশে উচ্ছ্বাস</h1>
+        <h1>বাংলাদেশে উচ্ছ্বাস new </h1>
         <h1>get relative url javascript</h1>
       </main>
     </div>
